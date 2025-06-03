26d301165d403893c0d1f895d33b0d278a587b05
Megaduck14 <giorgi.chubinishvili@gmail.com>
2025-06-03 19:23:08 +0400

    Add files via upload

---
+++ homework1/assignment1.py
@@ -0,0 +1,17 @@
+first_name=("gela ")
+last_name=("barkalaia")
+full_name=first_name + last_name
+print(full_name)
+print( full_name, type(full_name))
+age=20
+print("age is " , age , " and it's " , type(age) )
+temperature=20.3
+print("temperature " , type(temperature))
+is_hot=temperature>=30
+print("temperature is hot" , is_hot)
+print("is_hot" , type(is_hot))
+a=10
+b=10.2
+c="word"
+d=(a>b)
+print(d)
